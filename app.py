c91d9f1edffc3ba7e2930649e337eae637d412ca
Andres Gonzalez <gonche1124@gmail.com>
2017-01-01 21:33:06 +0000

    added display page

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ def main():
 def index():
     return render_template('index.html')
 
+@app.route('/displayData')
+def displayData():
+    return render_template('displayData.html')
+
 if __name__ == '__main__':
     app.run(port=33507)
 
