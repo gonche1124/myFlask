2382f9cc488c0ceb4caa63e7d5f4a2fc688330b1
Andres Gonzalez <gonche1124@gmail.com>
2017-01-01 02:13:21 +0000

    Same as above

--- app.py
+++ app.py
@@ -1,15 +1,7 @@
-from flask import Flask, render_template, request, redirect
-
+from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-  return redirect('/index')
+def hello_world():
+    return 'Hello, World!'
 
-@app.route('/index')
-def index():
-  return render_template('index.html')
-
-if __name__ == '__main__':
-  app.run(host='0.0.0.0')
-#app.run(port=33507)
